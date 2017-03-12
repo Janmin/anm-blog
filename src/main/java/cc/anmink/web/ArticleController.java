d9e77aa38bbda0e92fb6f2b0eca93a1b17ac0ae9
xiezijian <xiezijian@corp-ci.com>
2017-03-12 22:56:07 +0800

    测试目录写法

--- src/main/java/cc/anmink/web/ArticleController.java
+++ src/main/java/cc/anmink/web/ArticleController.java
@@ -1,18 +1,24 @@
 package cc.anmink.web;
 
 import cc.anmink.entity.SysArticle;
+import cc.anmink.entity.SysSetting;
 import cc.anmink.responese.MyResponse;
 import cc.anmink.service.ArticleService;
 import cc.anmink.service.SettingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Null;
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -67,25 +73,33 @@ public class ArticleController {
         return settingService.updateAllById(count_code, copy_right, description, icp, site_status, (long) 1);
     }
 
+    @RequestMapping("/admin/test2")
+    @ResponseBody
+    public void hello() {
+        String j = String.valueOf(ClassLoader.getSystemResource("../"));
+        System.out.println(j);
+    }
+
     @RequestMapping("/admin/upload")
     @ResponseBody
-    public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+    public MyResponse handleFileUpload(@RequestParam("file") MultipartFile file) {
         if (!file.isEmpty()) {
             try {
-                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File("E:/WorkSpace/anm-blog/" + file.getOriginalFilename())));
+                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(file.getOriginalFilename())));
                 out.write(file.getBytes());
                 out.flush();
                 out.close();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
-                return "上传失败," + e.getMessage();
+                // return "上传失败," + e.getMessage();
+                return new MyResponse<Null>(400, e.getMessage(), null);
             } catch (IOException e) {
                 e.printStackTrace();
-                return "上传失败," + e.getMessage();
+                return new MyResponse<Null>(400, e.getMessage(), null);
             }
-            return "上传成功";
+            return new MyResponse<String>(200, "success", file.getOriginalFilename());
         } else {
-            return "上传失败，因为文件是空的.";
+            return new MyResponse<Null>(400, "fail", null);
         }
     }
 }
